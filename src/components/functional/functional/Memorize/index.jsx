a86e6c2a21c6bfe8f4b52e71d3e8b3a63f0e1336
Mejla Ugljanin <ugljanin.mejla@hotmail.com>
2022-08-13 18:23:39 +0200

    hooks

--- src/components/functional/functional/Memorize/index.jsx
+++ src/components/functional/functional/Memorize/index.jsx
@@ -1,36 +1,147 @@
-import { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback, memo, useMemo } from 'react';
 
 const Memorize = props => {
-  const [text, setText] = useState();
+  const [text, setText] = useState(1);
 
-  const myCallback = useCallback(
-    e => {
-      setText(e.target.value);
-      console.log(e.target.value);
-    },
-    [setText]
+  const array = useMemo(() => {
+    return [
+      { id: 'sdf' },
+      { id: '1df' },
+      { id: '2df' },
+      { id: '3df' },
+      { id: '4df' },
+      { id: '5df' },
+      { id: '6sf' },
+    ];
+  }, []);
+
+  const computedArray = useMemo(() => {
+    array.filter(item => {
+      return item.id.includes('s');
+    });
+  }, []);
+
+  return (
+    <div>
+      <p onClick={() => setText(text + 1)}>{text}</p>
+      {array.map(item => {
+        return <p id={item.id}>{item.id}</p>;
+      })}
+    </div>
   );
+};
 
-  useEffect(() => {
-    console.log('here');
+const MyText = React.memo(props => {
+  return <p>Nesto</p>;
+});
+
+const List = React.memo(props => {
+  return [1, 2, 3, 4].map(item => {
+    return (
+      <div>
+        <span>{item}</span>
+        <button onClick={props.onClick}>{item}</button>
+      </div>
+    );
   });
+});
 
-  return (
-    <div>
-      <MyInput onChange={myCallback} />
-      <MyInput onChange={myCallback} />
-      <MyInput onChange={myCallback} />
-    </div>
-  );
-};
+// const Memorize = props => {
+//   const [text, setText] = useState(1);
 
-const MyInput = props => {
-  return (
-    <div>
-      <label htmlFor="">My text</label>
-      <input type="text" onChange={props.onChange} />
-    </div>
-  );
-};
+//   const change = useCallback(e => {
+//     console.log('first');
+//     setText(e.target.innerText);
+//   }, []);
+
+//   return (
+//     <div>
+//       <p onClick={() => setText(text + 1)}>{text}</p>
+//       <List onClick={change} />
+//     </div>
+//   );
+// };
+
+// const Memorize = props => {
+//   const [text, setText] = useState(1);
+
+//   const change = useCallback(
+//     e => {
+//       setText(e.target.value);
+//       console.log(text);
+//     },
+//     [text]
+//   );
+
+//   return (
+//     <div>
+//       <p>{text}</p>
+//       <MyInput onChange={change} />
+//       <MyInput onChange={change} />
+//       <MyInput onChange={change} />
+//       <MyInput onChange={change} />
+//     </div>
+//   );
+// };
+
+// const Memorize = props => {
+//   const [text, setText] = useState(1);
+
+//   // const change = useCallback(e => {
+//   //   setText(e.target.value);
+//   //   console.log(text);
+//   // }, []);
+
+//   const click = useCallback(() => {
+//     console.log('nesto');
+//   }, []);
+
+//   useEffect(() => {
+//     click();
+//   }, [click]);
+
+//   return (
+//     <div>
+//       <p>{text}</p>
+//       <MyInput onChange={click} />
+//     </div>
+//   );
+// };
+
+// const Memorize = props => {
+//   const [text, setText] = useState(1);
+
+//   // const change = useCallback(e => {
+//   //   setText(e.target.value);
+//   //   console.log(text);
+//   // }, []);
+
+//   const change = useCallback(
+//     e => {
+//       setText(e.target.value);
+//       console.log(text);
+//     },
+//     [text]
+//   );
+
+//   return (
+//     <div>
+//       <p>{text}</p>
+//       <MyInput onChange={change} />
+//       <MyInput onChange={change} />
+//       <MyInput onChange={change} />
+//       <MyInput onChange={change} />
+//     </div>
+//   );
+// };
+
+// const MyInput = props => {
+//   return (
+//     <div>
+//       <span>NEsto</span>
+//       <input type="text" onChange={props.change} />
+//     </div>
+//   );
+// };
 
 export default Memorize;
